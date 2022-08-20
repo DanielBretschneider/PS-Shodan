8d7c835530d225490aa278d429b6ba96cb1f1b74
Daniel Bretschneider <daniel@bretschneider.cc>
2022-08-20 13:21:59 +0200

    Added Header Comment

---
+++ PS-Shodan.ps1
@@ -0,0 +1,17 @@
+# Script CBH-1
+<# .SYNOPSIS
+     PS-Shodan is a open source tool for performing Shodan API requests via Powershell, including automated scans that can be sent out via email. 
+.DESCRIPTION
+     Several goals are set up:
+     * performing standard API requests and formatted Output
+     * CMD-Arguemnts, f.e. PS-Shodan -Host 127.0.0.1
+     * Save scan results in scan folder
+     * Perfom automated tests
+     * optionally send scan results from automated test via email 
+
+     IMPORTANT: Please enter API Key in key.txt file!
+.NOTES
+     Author     : Daniel Bretschneider - daniel.bretschneider@frequentis.com
+.LINK
+     http://tfl09.blogspot.com
+#>
